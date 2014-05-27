be06bedc201a10ed35432b477dd4a5bbe2c08dda
Maksym Borodin <borodin.maksym@gmail.com>
2014-05-27 14:45:04 +0200

    dev config for FB provider

--- wsgi/config.py
+++ wsgi/config.py
@@ -1,13 +1,24 @@
-import os
-import sys
-
-if 'OPENSHIFT_REPO_DIR' in os.environ:
-     settings = {
-          'static_path' : os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/static'),
-          'template_path' : os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/templates'),
-     }
-else:
-     settings = {
-          'static_path' : os.path.join(os.getcwd(), 'wsgi/static'),
-          'template_path' : os.path.join(os.getcwd(), 'wsgi/templates'),
-     }
+config = {
+    'production': {
+        'providers': {
+            'facebook': {
+                'appId': '',
+                'secret': ''
+            }
+        }
+    },
+    'dev': {
+        'providers': {
+            'facebook': {
+                'appId': '499586600168548',
+                'secret': '4d2685de6954f66399c0728e9258a104',
+                'request': {
+                    'scope': 'public_profile,email,user_friends'
+                },
+                'url': 'https://graph.facebook.com',
+                'token_url': '/oauth/access_token',
+                'authorize_url': 'https://www.facebook.com/dialog/oauth'
+            }
+        }
+    }
+}
