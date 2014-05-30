80274bdc175c65fd274b63757c6222d19f8a890b
Maksym Borodin <borodin.maksym@gmail.com>
2014-05-30 18:26:23 +0200

    added model

--- wsgi/vasco/__init__.py
+++ wsgi/vasco/__init__.py
@@ -1,7 +1,9 @@
 from .application import app
 import login
 import localization
+from model import db
 
 make_pep8_happy = app
 make_pep8_happy = login
 make_pep8_happy = localization
+make_pep8_happy = db
